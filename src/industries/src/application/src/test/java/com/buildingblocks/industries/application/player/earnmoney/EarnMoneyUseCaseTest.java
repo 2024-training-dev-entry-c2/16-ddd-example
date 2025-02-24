3907b2345c52f0097023792fcb56d18df69d1c02
danielaser <daniela.serrano.dev@gmail.com>
2025-02-23 20:27:08 -0500

    add earn money for player usecase

---
+++ src/industries/src/application/src/test/java/com/buildingblocks/industries/application/player/earnmoney/EarnMoneyUseCaseTest.java
@@ -0,0 +1,42 @@
+package com.buildingblocks.industries.application.player.earnmoney;
+
+import com.buildingblocks.industries.application.shared.repositories.IEventRepository;
+import com.buildingblocks.industries.domain.player.events.EarnedMoney;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+class EarnMoneyUseCaseTest {
+    private EarnMoneyUseCase useCase;
+    private IEventRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        repository = Mockito.mock(IEventRepository.class);
+        useCase = new EarnMoneyUseCase(repository);
+    }
+
+    @Test
+    void executeSuccess() {
+        Mockito.when(repository.findEventsByAggregateId(Mockito.anyString()))
+                .thenReturn(Flux.just(
+                        new EarnedMoney("player-123", 50, 70)
+                ));
+
+        EarnMoneyRequest request = new EarnMoneyRequest(
+                "1","player-123",  20, 70
+        );
+
+        Mockito.doNothing().when(repository).save(Mockito.any());
+
+        StepVerifier
+                .create(useCase.execute(request))
+                .consumeErrorWith(error -> {
+                    System.out.println("Error: " + error.getMessage());
+                    error.printStackTrace();
+                })
+                .verify();
+    }
+}

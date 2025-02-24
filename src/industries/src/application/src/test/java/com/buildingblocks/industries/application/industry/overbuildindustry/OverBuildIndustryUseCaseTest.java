07d3d632cd94541433acfeecf416daf3a2934fde
danielaser <daniela.serrano.dev@gmail.com>
2025-02-23 20:11:23 -0500

    add test overbuild industry usecase

---
+++ src/industries/src/application/src/test/java/com/buildingblocks/industries/application/industry/overbuildindustry/OverBuildIndustryUseCaseTest.java
@@ -0,0 +1,40 @@
+package com.buildingblocks.industries.application.industry.overbuildindustry;
+
+import com.buildingblocks.industries.application.shared.repositories.IEventRepository;
+import com.buildingblocks.industries.domain.industry.events.BuiltIndustry;
+import com.buildingblocks.industries.domain.industry.events.OverBuiltIndustry;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+class OverBuildIndustryUseCaseTest {
+    private OverBuildIndustryUseCase useCase;
+    private IEventRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        repository = Mockito.mock(IEventRepository.class);
+        useCase = new OverBuildIndustryUseCase(repository);
+    }
+
+    @Test
+    void executeSuccess() {
+        Mockito.when(repository.findEventsByAggregateId(Mockito.anyString()))
+                .thenReturn(Flux.just(
+                        new BuiltIndustry("Iron Work", 2,"Liverpool", 10, "Coal", 2, false, "Canals", false),
+                        new OverBuiltIndustry("industry-123", "Iron Work", 2, 15)
+                ));
+
+        OverBuildIndustryRequest request = new OverBuildIndustryRequest("123", "industry-123", "Iron Work",2,  15);
+
+        StepVerifier
+                .create(useCase.execute(request))
+                .consumeErrorWith(error -> {
+                    System.out.println("Error: " + error.getMessage());
+                    error.printStackTrace();
+                })
+                .verify();
+    }
+}

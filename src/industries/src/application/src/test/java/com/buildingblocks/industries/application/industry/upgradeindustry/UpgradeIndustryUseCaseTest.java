e04f7fc983227fb208a2d94ae26e934ca3275e16
danielaser <daniela.serrano.dev@gmail.com>
2025-02-23 20:14:15 -0500

    add test upgrade industry usecase

---
+++ src/industries/src/application/src/test/java/com/buildingblocks/industries/application/industry/upgradeindustry/UpgradeIndustryUseCaseTest.java
@@ -0,0 +1,42 @@
+package com.buildingblocks.industries.application.industry.upgradeindustry;
+
+import com.buildingblocks.industries.application.shared.repositories.IEventRepository;
+import com.buildingblocks.industries.domain.industry.events.BuiltIndustry;
+import com.buildingblocks.industries.domain.industry.events.UpgradedIndustry;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+class UpgradeIndustryUseCaseTest {
+    private UpgradeIndustryUseCase useCase;
+    private IEventRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        repository = Mockito.mock(IEventRepository.class);
+        useCase = new UpgradeIndustryUseCase(repository);
+    }
+
+    @Test
+    void executeSuccess() {
+        Mockito.when(repository.findEventsByAggregateId(Mockito.anyString()))
+                .thenReturn(Flux.just(
+                        new BuiltIndustry("Iron Work", 2,"Liverpool", 10, "Coal", 2, false, "Canals", false),
+                        new UpgradedIndustry("industry-123", "Iron Work", 3, "Liverpool", false, "Coal", 2, 20, 4, true, "Canals")
+                ));
+
+        UpgradeIndustryRequest request = new UpgradeIndustryRequest(
+                "1", "industry-123", "Iron Work", 3, "Liverpool", false, "Coal", 2, 20, 4, true, "Canals"
+        );
+
+        StepVerifier
+                .create(useCase.execute(request))
+                .consumeErrorWith(error -> {
+                    System.out.println("Error: " + error.getMessage());
+                    error.printStackTrace();
+                })
+                .verify();
+    }
+}

2a15f52b865d3fba03742d48b0a7c3a066f97f39
danielaser <daniela.serrano.dev@gmail.com>
2025-02-23 20:05:46 -0500

    add test flip industry usecase

---
+++ src/industries/src/application/src/test/java/com/buildingblocks/industries/application/industry/flipindustry/FlipIndustryUseCaseTest.java
@@ -0,0 +1,40 @@
+package com.buildingblocks.industries.application.industry.flipindustry;
+
+import com.buildingblocks.industries.application.shared.repositories.IEventRepository;
+import com.buildingblocks.industries.domain.industry.events.BuiltIndustry;
+import com.buildingblocks.industries.domain.industry.events.FlippedIndustry;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+class FlipIndustryUseCaseTest {
+    private FlipIndustryUseCase useCase;
+    private IEventRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        repository = Mockito.mock(IEventRepository.class);
+        useCase = new FlipIndustryUseCase(repository);
+    }
+
+    @Test
+    void executeSuccess() {
+        Mockito.when(repository.findEventsByAggregateId(Mockito.anyString()))
+                .thenReturn(Flux.just(
+                        new BuiltIndustry("Iron Work", 2, "Liverpool", 10, "Coal", 2, false, "Canals", false),
+                        new FlippedIndustry("industry-123", "Liverpool", "Coal", 2, 5)
+                ));
+
+        FlipIndustryRequest request = new FlipIndustryRequest("123", "industry-123", "Liverpool", "Coal", 2, 5);
+
+        StepVerifier
+                .create(useCase.execute(request))
+                .consumeErrorWith(error -> {
+                    System.out.println("Error: " + error.getMessage());
+                    error.printStackTrace();
+                })
+                .verify();
+    }
+}

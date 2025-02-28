9db9302fb134c6fc2e416c02fdd217c017aa158b
danielaser <daniela.serrano.dev@gmail.com>
2025-02-27 19:13:27 -0500

    feat(main): add conditions to resourcemarket mapper

--- src/industries/src/application/src/main/java/com/buildingblocks/industries/application/shared/ResourceMarketUtils/ResourceMarketMapper.java
+++ src/industries/src/application/src/main/java/com/buildingblocks/industries/application/shared/ResourceMarketUtils/ResourceMarketMapper.java
@@ -2,6 +2,8 @@ package com.buildingblocks.industries.application.shared.ResourceMarketUtils;
 
 import com.buildingblocks.industries.domain.resourceMarket.ResourceMarket;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class ResourceMarketMapper {
@@ -9,7 +11,8 @@ public class ResourceMarketMapper {
     public static ResourceMarketResponse mapToResourceMarket(ResourceMarket resourceMarket) {
         return new ResourceMarketResponse(
                 resourceMarket.getIdentity().getValue(),
-                resourceMarket.getTradeExchange().stream().map(trade ->
+                resourceMarket.getTradeExchange() != null ?
+                        resourceMarket.getTradeExchange().stream().map(trade ->
                         new ResourceMarketResponse.TradeExchange(
                                 trade.getIdentity().getValue(),
                                 trade.getTradeType().name(),
@@ -18,9 +21,10 @@ public class ResourceMarketMapper {
                                 trade.getResourcePrice().getValue(),
                                 trade.getAvailableResources().getValue()
                         )
-                ).collect(Collectors.toList()),
-                resourceMarket.getAvailableResources().getValue(),
-                resourceMarket.getResourcePrice().getValue()
+                ).collect(Collectors.toList()) :
+                        Collections.emptyList(),
+                resourceMarket.getAvailableResources() != null ? resourceMarket.getAvailableResources().getValue() : List.of(),
+                resourceMarket.getResourcePrice() != null ? resourceMarket.getResourcePrice().getValue() : 0
         );
     }
 }
